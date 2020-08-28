5947866ddc3ef3df100d4fbb68de8fc8781e0268
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-08-28 22:22:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+
+class Cat {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    returns`${this.name} says meow!`;
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    returns`${this.name} says woof!`;
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name;
+    this.sex = sex;
+  }
+
+  speak() {
+    if (sex === "male") {
+      return `It's me! ${this.name}, the parrot!`;
+    } else {
+      return `${name} says squawk!`;
+    }
+  }
+}
